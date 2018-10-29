eab66f0cccaa56749641c73fcf029cf15fed2aa3
Chris Anderson <chris@fauna.com>
2018-10-29 13:13:12 -0700

    try removing packages

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -1,4 +1,7 @@
+import faunadb from 'faunadb' /* Import faunaDB sdk */
+
 export function handler(event, context, callback) {
+  isNaN(faunadb)
   const payload = JSON.parse(event.body);
   const user = payload.user;
   console.log(`Netlify user ${user}`)
